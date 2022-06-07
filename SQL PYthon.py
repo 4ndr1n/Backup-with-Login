09a3af841035291b2fe953bf85e0e1f11e02d4ef
4ndr1n <47792683+4ndr1n@users.noreply.github.com>
2022-06-07 10:43:53 +0200

    Update SQL PYthon.py

--- SQL PYthon.py
+++ SQL PYthon.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import shutil
 import time
 
+# Creates the connection to the SQLdatabase
 mydb = mysql.connector.connect(
   user="root",
   password="asdfasdf",
@@ -11,115 +12,149 @@ mydb = mysql.connector.connect(
   database="Schulprojekt"
 ) 
 
+# Alias for the cursor for efficiency
 mc = mydb.cursor()
 
 mc.execute("USE Schulprojekt;")
 
+# Vars
 now = datetime.now()
 date = now.strftime("%d-%m-%Y_%H:%M")
 user = "Andrin"
-i = 10
+e = 1
 
+# Methods
+# returns all the filenames in a specified path
 def scantree(path):
-    for i in x:
+    for i in path:
         if i.is_dir(follow_symlinks=False):
             yield from os.scandir(i.path)
         else:
             yield i
-    
 
-print("Bist du dir sicher das du ein Backup machen möchtest?")
+# Copy all specified directories and inserts then into the database
+def copyAll():
+  shutil.copytree("/Users/{}/Desktop", "/Volumes/Untitled 2/copy".format(user))
 
-d = 1
+  shutil.copytree("/Users/{}/Pictures".format(user), "/Volumes/Untitled 2/backup/{}".format(date))
 
-arr = []
-# os.stat(i).st_ctime
-while d == 1:
-  uip = input("Bestätige mit [J]a oder [N]ein ")
-  if uip == "J":
-    d = 0
+  shutil.copytree("/Users/{}/Downloads".format(user), "/Volumes/Untitled 2/backup/{}".format(date))
 
-    try:
-      os.chdir("/Volumes/Untitled 2")
-    except:
-      print("""Error: Kein USB gefunden.
-        
-        Script wird beendet in 10 sec.""")
-        
-    if os.getcwd() == "/Volumes/Untitled 2":
-      try:
-        os.chdir("/Volumes/Untitled 2/backup")
-      except:
-        print("Error: Backup wurde nicht gefunden")
+  shutil.copytree("/Users/{}/Music".format(user), "/Volumes/Untitled 2/backup/{}".format(date))
 
-      if os.getcwd() == "/Volumes/Untitled 2/backup":
-        # Neue Funktion zum Alte versionen zu löschen, wenn zu wenig Speicherplatz vorhanden ist.
+  shutil.copytree("/Users/{}/Documents".format(user), "/Volumes/Untitled 2/backup/{}".format(date))
 
-        quip = input ("Willst du ein Backup von allen Ordner machen? Bestätige mit Ja oder Nein: ")
+  print("Backup abgeschlossen")
 
-        if quip == "Ja":
-          shutil.copytree("/Users/{}/Desktop", "/Volumes/Untitled 2/copy".format(user))
+# copies the directory Downloads and enters the copied files into the DB
+def copyDownloads():
+  shutil.copytree("/Users/{}/Downloads".format(user), "/Volumes/Untitled 2/backup/{}-DL".format(date))
 
-          shutil.copytree("/Users/{}/Pictures".format(user), "/Volumes/Untitled 2/backup/{}".format(date))
-          
-          shutil.copytree("/Users/{}/Downloads".format(user), "/Volumes/Untitled 2/backup/{}".format(date))
+  for i in scantree("/Users/Andrin/Downloads"):
+    input = "INSERT INTO Eintraege (Zeit,Dateiname) VALUES ({});"
 
-          shutil.copytree("/Users/{}/Music".format(user), "/Volumes/Untitled 2/backup/{}".format(date))
-
-          shutil.copytree("/Users/{}/Documents".format(user), "/Volumes/Untitled 2/backup/{}".format(date))
-
-          print("Backup abgeschlossen")
-
-        elif quip == "Nein":
-          dip = input("Willst du ein Backup von Dokumente machen? Bestätige mit Ja oder Nein: ")
-
-        if dip == "Ja":
-          shutil.copytree("/Users/{}/Documents".format(user),  "/Volumes/Untitled 2/backup/{}-DK".format(date))
-          
-          print("Backup abgeschlossen")
-
-        elif dip == "Nein":
-          dlip = input("Willst du ein Backup von Downloads machen? Bestätige mit Ja oder Nein: ")
-
-        if dlip == "Ja":
-          shutil.copytree("/Users/{}/Downloads".format(user), "/Volumes/Untitled 2/backup/{}-DL".format(date))
-
-          for i in scantree("/Users/Andrin/Downloads"):
-            input = "INSERT INTO Eintraege (Zeit,Dateiname) VALUES ({});"
-
-            mc.execute(input.format(date,i))
-            mydb.commit()
+  mc.execute(input.format(date,i))
+  mydb.commit()
 
 
-          print("Backup abgeschlossen")
-      else:
-        for i in range(10):
-          print("Countdown ends in {}".format(10-i))
+  print("Backup abgeschlossen")
+
+# copies the directory Documents and enters the copied files into the DB
+def copyDocuments():
+  shutil.copytree("/Users/{}/Documents".format(user),  "/Volumes/Untitled 2/backup/{}-DK".format(date))
+
+  print("Backup abgeschlossen")
+
+# Counts to ten
+def whileCounter():
+  i = 10
+  while (i > 0):
+          print("Countdown endet in {}".format(i))
+          i-=1
           time.sleep(1)
-          print("Script wird beendet.")
 
+# Counts to ten but differently
+def forCounter():
+  for i in range(10):
+            print("Countdown ends in {}".format(10-i))
+            time.sleep(1)
+            print("Script wird beendet.")
 
-    else:
-      while (i > 0):
-        print("Countdown endet in {}".format(i))
-        i-=1
-        time.sleep(1)
+def checkUSB():
+  try:
+    os.chdir("/Volumes/Untitled 2")
+  except:
+    print("""Error: Kein USB gefunden.
       
-  elif uip == "N":
-    print("Bye")
+      Script wird beendet in 10 sec.""")
+      
+
+# manages the backup process
+def backup():
+  d = 1
+  print("Bist du dir sicher, dass du ein Backup machen möchtest?")
+  arr = []
+  # os.stat(i).st_ctime
+  while d == 1:
+    uip = input("Bestätige mit [J]a oder [N]ein ")
+    if uip == "J":
+      d = 0
+      
+      checkUSB()
+
+      if os.getcwd() == "/Volumes/Untitled 2":
+        try:
+          os.chdir("/Volumes/Untitled 2/backup")
+        except:
+          print("Error: Backup wurde nicht gefunden")
+
+        if os.getcwd() == "/Volumes/Untitled 2/backup":
+          # Neue Funktion zum Alte versionen zu löschen, wenn zu wenig Speicherplatz vorhanden ist.
+
+          quip = input ("Willst du ein Backup von allen Ordner machen? Bestätige mit Ja oder Nein: ")
+
+          if quip == "Ja":
+            copyAll()
+
+          elif quip == "Nein":
+            dip = input("Willst du ein Backup von Dokumente machen? Bestätige mit Ja oder Nein: ")
+
+          if dip == "Ja":
+            copyDocuments()
+          elif dip == "Nein":
+            dlip = input("Willst du ein Backup von Downloads machen? Bestätige mit Ja oder Nein: ")
+
+          if dlip == "Ja":
+            copyDownloads()
+        else:
+          forCounter()
+      else:
+        whileCounter()
+        
+    elif uip == "N":
+      print("Bye")
+      d = 0
+    else:
+      print("Was?")
+
+
+
+  input = "INSERT INTO Eintraege (Zeit,Dateiname) VALUES ({});"
+
+  mc.execute(input.format())
+
+  mydb.commit()
+
+  mc.execute("SELECT * FROM dis;")
+
+  for x in mc:
+    print(x)
+
+
+while e == 1:
+  wuip = input("Willst du ein [b]ackup machen oder sie [d]urchsuchen?")
+  if wuip == "b":
     d = 0
-  else:
-    print("Was?")
-
-
-
-input = "INSERT INTO Eintraege (Zeit,Dateiname) VALUES ({});"
-
-mc.execute(input.format())
-
-mydb.commit()
-
-mc.execute("SELECT * FROM dis;")
-
-for x in mc:
-  print(x)
+    backup()
+  elif wuip == "d":
+    d = 0
